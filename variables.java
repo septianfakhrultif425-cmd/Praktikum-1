ae30e68039efbc2c17f12f6b1b27aef9f0902c39
septianfakhrultif425-cmd <septian.fakhrul.tif425@polban.ac.id>
2026-02-12 10:37:40 +0700

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static final double CM_PER_INCH = 2.54;
+    public static void main(String[] args) {
+        // Bagian 1: Konstanta Lokal
+        // final double CM_PER_INCH_LOKAL = 2.54; 
+
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+
+        System.out.println("Paper size in centimeters: " + 
+            paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+    }
+}
+
