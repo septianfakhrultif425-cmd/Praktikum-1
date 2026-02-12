5e52d91a5fe73398057016d257a09351581000dd
septianfakhrultif425-cmd <septian.fakhrul.tif425@polban.ac.id>
2026-02-12 21:16:39 +0700

    Add files via upload

---
+++ FloatingPoint.java
@@ -0,0 +1,9 @@
+class FloatingPoint {
+    public static void main(String[] args) { 
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+        
+        // Menambahkan perintah print agar variabel nx digunakan
+        System.out.println("Hasil pembulatan: " + nx);
+    }
+}
